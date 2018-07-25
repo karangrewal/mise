1c9098d341eacfafd59b0b54eb6ac82123dc5003
karangrewal <karan_gre_wal@hotmail.com>
2018-07-25 08:28:04 -0400

    two class classification

--- two_class_regression.py
+++ two_class_regression.py
@@ -1,54 +1,69 @@
 """
 Linear 2-class classification
+
+This classifier distinguishes between samples drawn from different normal
+distributions
 """
 import numpy as np
 
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 INPUT_DIM = 10
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.01
 NUM_ITERATIONS = 50
 
+
 def forward(w, b, X):
-    return 1. / (1. + np.exp(-np.dot(X, w) - np.tile(b, (32)), dtype=np.float64))
+    # Compute 
+    # Hint: Look up the functions np.exp, np.dot, np.tile, they will be useful
+    # YOUR CODE HERE
+
+########## YOU DON'T NEED TO KNOW HOW THESE WORK, BUT THEY WORK! ;) ###########
 
 def gradient(w, b, X, Y):
+    """
+    Compute the gradient of the cross-entropy loss and return a dictionary
+    that contains the gradients
+    """
     forward_prop = forward(w, b, X)
     dw, db = np.zeros((INPUT_DIM)), 0.
     for n in range(X.shape[0]):
         for d in range(INPUT_DIM):
-            dw[d] += (1. - forward_prop[n]) * X[n, d] * Y[n, 1] + forward_prop[n] * X[n, d] * (1. - Y[n, 1])
-        db += (1. - forward_prop[n]) * Y[n, 1] + forward_prop[n] * (1. - Y[n, 1])
+            dw[d] += (1. - forward_prop[n]) * X[n, d] * Y[n, 0] + forward_prop[n] * X[n, d] * (1. - Y[n, 0])
+        db += (1. - forward_prop[n]) * Y[n, 0] + forward_prop[n] * (1. - Y[n, 0])
     return {"dw":dw, "db":db}
 
 def get_batch():
-    X_a = np.random.normal(1., 1., (BATCH_SIZE / 2, INPUT_DIM))
-    X_b = np.random.normal(-1., 1., (BATCH_SIZE / 2, INPUT_DIM))
+    """
+    Draw samples from two normal distributions with means 1 and -1
+    """
+    X_a = np.random.normal(1., 1.3, (BATCH_SIZE / 2, INPUT_DIM))
+    X_b = np.random.normal(-1., 1.3, (BATCH_SIZE / 2, INPUT_DIM))
     X = np.concatenate((X_a, X_b), axis=0)
     Y_top = np.concatenate((np.ones((BATCH_SIZE / 2, 1)), np.zeros((BATCH_SIZE / 2, 1))), axis=1)
     Y_bottom = np.concatenate((np.zeros((BATCH_SIZE / 2, 1)), np.ones((BATCH_SIZE / 2, 1))), axis=1)
     Y = np.concatenate((Y_top, Y_bottom), axis=0)
     return X, Y
 
+###############################################################################
+
 if __name__ == "__main__":
+    # Initialize weights and bias randomly
     w = np.random.randn(INPUT_DIM)
     b = np.random.randn()
 
     # Training
     for it in range(NUM_ITERATIONS):
+        # Get a "batch" of examples
         X, Y = get_batch()
-        grad = gradient(w, b, X, Y)
-        w = w - LEARNING_RATE * grad["dw"]
-        b = b - LEARNING_RATE * grad["db"]
+
+        # Compute gradient, then update weights and bias
+        # YOUR CODE HERE
         
-        # Validate
-        # if it % 25 == 0:
-        if True:
-            scores = list()
-            for n in range(10):
-                X, Y = get_batch()
-                Y_out = np.round(forward(w, b, X))
-                Y = 1. - np.argmax(Y, axis=1)
-                # if n == 3:
-                #     print(np.concatenate((Y.reshape(-1,1), Y_out.reshape(-1,1)), axis=1))
-                scores.append(100. * np.sum((Y == Y_out)) / BATCH_SIZE)
-            print("Iter {}: Accuracy {}%".format(it, np.mean(scores)))
+        # Check accuracy of our model
+        scores = list()
+        for n in range(10):
+            X, Y = get_batch()
+            Y_out = np.round(forward(w, b, X))
+            Y = np.argmax(Y, axis=1)
+            scores.append(100. * np.sum((Y == Y_out)) / BATCH_SIZE)
+        print("Iter {}: Accuracy {}%".format(it, np.mean(scores)))
